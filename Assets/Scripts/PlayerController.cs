300b568221e2309b699755394637a1132a6ae733
pincuiulia <pincu.iulia28@gmail.com>
2024-04-23 18:56:55 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    public LayerMask solidObjectsLayer; // Layer-ul pentru obiectele solide
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        Vector2 newPosition = rb.position + movement * (moveSpeed * Time.fixedDeltaTime);
+
+        // Verificăm dacă noua poziție este walkable
+        if (isWalkable(newPosition))
+        {
+            rb.MovePosition(newPosition);
+        }
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        // Verificăm dacă există un obiect solid în zona țintă
+        Collider2D hitCollider = Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer);
+        
+        // Returnăm true dacă nu există niciun obiect solid în zona țintă
+        return hitCollider == null;
+    }
+}
