fe67c83f7213113c071b0f8f12a4a38146d2c8c9
alexandrazamfirescu <alexandra.zamfirescu1@s.unibuc.ro>
2024-05-29 03:58:34 +0300

    Revert "SHOPPING MAP"
    This reverts commit ab46b017a1fa5a81bf2c01956eabfc45c280d31b.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,26 +20,16 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
-
-        Debug.Log("Awake: Initialized components");
     }
 
     private void OnEnable()
     {
         playerControls.Enable();
-        Debug.Log("OnEnable: Player controls enabled");
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-        Debug.Log("OnDisable: Player controls disabled");
     }
 
     private void Update()
     {
         PlayerInput();
-        Debug.Log($"Update: Movement input - {movement}");
     }
 
     private void FixedUpdate()
@@ -54,8 +44,6 @@ public class PlayerController : MonoBehaviour
 
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
-
-        Debug.Log($"PlayerInput: moveX = {movement.x}, moveY = {movement.y}");
     }
 
     private void Move()
@@ -66,11 +54,6 @@ public class PlayerController : MonoBehaviour
         if (isWalkable(newPosition))
         {
             rb.MovePosition(newPosition);
-            Debug.Log($"Move: New position = {newPosition}");
-        }
-        else
-        {
-            Debug.Log($"Move: Position not walkable = {newPosition}");
         }
     }
 
@@ -82,21 +65,20 @@ public class PlayerController : MonoBehaviour
         if (mousePos.x < playerScreenPoint.x)
         {
             mySpriteRender.flipX = true;
-            Debug.Log("AdjustPlayerFacingDirection: Facing left");
         }
         else
         {
             mySpriteRender.flipX = false;
-            Debug.Log("AdjustPlayerFacingDirection: Facing right");
         }
     }
 
     private bool isWalkable(Vector3 targetPos)
     {
+        
         Collider2D hitCollider = Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer);
-        bool walkable = hitCollider == null;
-        Debug.Log($"isWalkable: targetPos = {targetPos}, walkable = {walkable}");
-        return walkable;
+        
+        
+        return hitCollider == null;
     }
 }
 
